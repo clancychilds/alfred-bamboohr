1a2f5e2761729cf9e77d6747d46fb483d40201de
Clancy Childs <cchilds@kingcontent.com>
2021-09-27 21:11:50 +0100

    Added Caching and display

--- bamboo.py
+++ bamboo.py
@@ -5,17 +5,29 @@ from workflow.util import set_config
 import requests
 import sys
 from dotenv import load_dotenv
-import os 
+import os
+import json
 
 load_dotenv()
 
-def main(wf):
+def fetchEmployees():
     api_key = os.environ.get('apiKey')
     org_name = os.environ.get('orgName')
     url = f"https://api.bamboohr.com/api/gateway.php/{org_name}/v1/employees/directory"
     headers = {'Accept': 'application/json'}
     r = requests.get(url, headers=headers, auth=(api_key, 'x'))
-    wf.logger.debug(r.json())
+    return(r.json()['employees'])
+
+def main(wf):
+    employees = wf.cached_data('employees', data_func=fetchEmployees, max_age=86400)
+    #wf.add_item(employees[0]['displayName'])
+    #map(lambda x: wf.logger.debug(x['displayName']), employees)
+    #map(lambda x: wf.add_item(x['displayName']), employees)
+    for employee in employees:
+        wf.add_item(employee['displayName'], subtitle=f"{employee['department']}: {employee['jobTitle']} - {employee['workEmail']}",
+                    copytext=employee['workEmail'], quicklookurl=employee['photoUrl'], arg=employee['photoUrl'],
+                    valid=True)
+    wf.send_feedback()
     
 
 if __name__ == '__main__':
